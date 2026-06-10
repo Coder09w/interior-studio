d419cdbf9ce7f517b82a09bbd2446842ed6832ab
Z User <z@container>
2026-06-10 08:04:56 +0000

    cda92e3c-2227-44be-a2b9-835b7901cc9c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,8 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from "@/components/providers/auth-provider";
-import { PostHogProvider } from "@/components/providers/posthog-provider";
 import { BetaBanner } from "@/components/beta-banner";
 import { FeedbackButton } from "@/components/feedback-button";
-import { CookieConsent } from "@/components/cookie-consent";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -112,14 +110,11 @@ export default function RootLayout({
           Skip to content
         </a>
         <AuthProvider>
-          <PostHogProvider>
-            <BetaBanner />
-            <div id="main-content">
-              {children}
-            </div>
-            <FeedbackButton />
-            <CookieConsent />
-          </PostHogProvider>
+          <BetaBanner />
+          <div id="main-content">
+            {children}
+          </div>
+          <FeedbackButton />
         </AuthProvider>
         <Toaster />
       </body>
